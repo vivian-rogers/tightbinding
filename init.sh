1f4159aa0f413977535367e9e3e7d198881d05c9
Vivian Rogers <viv@DESKTOP-JI4FGTM.localdomain>
2021-06-21 15:05:58 -0500

    More directions in init.sh

--- init.sh
+++ init.sh
@@ -26,5 +26,5 @@ mkdir -p pack/plugins/start && cd pack/plugins/start
 git clone git://github.com/JuliaEditorSupport/julia-vim.git
 
 
-echo "Install done (if run without errors), launch Vcxsrv with -1, default settings, and type"
+echo "Install done (if run without errors), launch Vcxsrv with -1, unclick native openGL, and type"
 echo "'julia -i Holmium.jl'"
